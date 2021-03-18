524b1eb87d3bfd4edea97b442e287a1183e851a0
ramsteven <ramsteven@gmail.com>
2021-03-18 12:45:39 -0500

    adding env variable

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,7 @@ require 'rubygems' if RUBY_VERSION < '1.9'
 require 'rest-client'
 require 'json'
 
-response = RestClient.get "https://mailtrap.io/api/v1/inboxes.json?api_token=#{Rails.application.credentials.mail_trap[:MAILTRAP_API_TOKEN]}"
+response = RestClient.get "https://mailtrap.io/api/v1/inboxes.json?api_token=#{ENV['MAILTRAP_API_TOKEN']}"
 first_inbox = JSON.parse(response)[0] # get first inbox
 
 ActionMailer::Base.delivery_method = :smtp
