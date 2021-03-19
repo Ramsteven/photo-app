d7838e60b7339834d614fa4dee12a52b55df010a
ramsteven <ramsteven@gmail.com>
2021-03-18 23:40:14 -0500

    adding stripe config and enviroments

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => Rails.application.credentials.stripe[:Publisheable_key],
-  :secret_key      => Rails.application.credentials.stripe[:Secret_key]
+  :publishable_key => ENV['STRIPE_TEST_PUNISHEABLE_KEY'],
+  :secret_key => ENV['STRIPE_TEST_SECRET_KEY']
 }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = Rails.application.credentials.stripe[:secret_key]
